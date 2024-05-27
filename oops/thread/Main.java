532eef4ef5d39f96064643ee28812b58fb38fc7b
Bhargav-Techin <148316999+Bhargav-Techin@users.noreply.github.com>
2024-05-27 20:50:48 +0530

    thread new commit

--- oops/thread/Main.java
+++ oops/thread/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static boolean stop = false;
 
     public static void main(String[] args) {
-        WriteOnFile newFile = new WriteOnFile("writer is writting", "File.txt");
+        WriteOnFile newFile = new WriteOnFile("Thread is writting", "D:\\java practice\\oops\\thread\\File.txt");
 
         newFile.start();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Something:  ");
-        sc.nextLine();
+        String inText = sc.nextLine();
+        newFile.writeInput(inText);
         Main.stop= true;
-        return;
     }
     
     
--- oops/thread/WriteOnFile.java
+++ oops/thread/WriteOnFile.java
@@ -6,37 +6,48 @@ import java.util.Scanner;
 
 public class WriteOnFile extends Thread{
 
-    private String mssg , fileName ;
+    private String  mssg , fileName ;
 
-    private String readFile(){
-        String currText = "";
-        try {
-            File myFile = new File(this.fileName);
-            Scanner myReader = new Scanner(myFile);
-            while (myReader.hasNextLine()){
-                currText = myReader.nextLine();
-                currText += "\n";
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-        return currText;
-    }
+//    private String readFile(){
+//        String currText = "";
+//        try {
+//            File myFile = new File(this.fileName);
+//            Scanner myReader = new Scanner(myFile);
+//            while (myReader.hasNextLine()){
+//                currText = myReader.nextLine();
+//                currText += "\n";
+//            }
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//        }
+//        return currText;
+//    }
 
     private void write(){
         while (Main.stop == false) {
             try {
-                String currText = readFile();
-                FileWriter myWriter = new FileWriter(fileName);
-                myWriter.write(currText + this.mssg);
+//                String currText = readFile();
+                FileWriter myWriter = new FileWriter(fileName, true);
+//                myWriter.write(currText + this.mssg);
+                myWriter.write(this.mssg + "\n");
                 myWriter.close();
 
-                Thread.sleep(1500);
+                Thread.sleep(2500);
             } catch (Exception e) {
                 // TODO: handle exception
             }
         }
-        return ;
+    }
+
+    public void writeInput(String text) {
+            try {
+//                String currText = readFile();
+                FileWriter myWriter = new FileWriter(fileName, true);
+//                myWriter.write(currText + text + "\n");
+                myWriter.write(text + "\n");
+                myWriter.close();
+            } catch (Exception e) {
+            }
     }
 
     public WriteOnFile(String mssg, String fileName){
